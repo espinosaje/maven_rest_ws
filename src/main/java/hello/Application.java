8227d685db4b6d9a02b7cf1a66ae9a222e8f3e68
espinosaje <espinosa.je@gmail.com>
2018-07-17 21:59:21 -0600

    Convert to a Traditional App
    Convert to a Traditional Application to be able to run it in Tomcat.
    Originally, it was a Spring Boot App, only able to run it on the embeded
    server.

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,12 +1,18 @@
 package hello;
-
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 }
