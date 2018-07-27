43f0d7090cf38cf008a087fc5873bd883ac32ee1
espinosaje <espinosa.je@gmail.com>
2018-07-26 23:31:22 -0600

    Added headers to request
    Added headers to request. Now it's compiling and running but it's
    returning NULL values.
    
    Response:
    Venue{id='null', name=null}

--- src/main/java/hello/setlist/VenueController.java
+++ src/main/java/hello/setlist/VenueController.java
@@ -1,5 +1,3 @@
-// Class is still in progress.
-
 package hello.setlist;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +8,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import java.util.Arrays;
+
 import hello.setlist.Venue;
 
 
@@ -19,14 +24,31 @@ public class VenueController {
 	//@Autowired
 	//private Venue venue;
 	
-	//todo: Need to add headers for the app key and JSON format
-	@GetMapping(path="/venue") // Map ONLY GET Requests
-	public @ResponseBody String addNewEvent (@RequestParam String cityName
+	@GetMapping(path="/venue")
+	public @ResponseBody String getVenue (@RequestParam String cityName
 			, @RequestParam String stateCode			
 			) {
 		String request = "https://api.setlist.fm/rest/1.0/search/venues"+"?cityName="+cityName+"&stateCode="+stateCode;		
 		RestTemplate restTemplate = new RestTemplate();
-        Venue venue = restTemplate.getForObject(request, Venue.class);
-        log.info(quote.toString());
+
+		System.out.println("request: "+request);
+		
+		
+		// set the headers for authentication
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		headers.set("x-api-key", "2f7b7e46-b66a-4281-8ac4-821cf1a03efb");
+		HttpEntity<String> entityReq = new HttpEntity<String>(headers);
+		
+	
+		//actual service call
+        //Venue venue = restTemplate.getForObject(request, Venue.class);
+		ResponseEntity<Venue> venue = (ResponseEntity<Venue>) restTemplate.exchange(request, HttpMethod.GET, entityReq, Venue.class);
+
+		System.out.println("venue.getStatusCode: "+venue.getStatusCode());
+		String result = (venue.getBody()).toString();
+		
+		return result;
 	}
 }
